824c980b2c5bcf45dd3a4191b085c52a74c6fc09
nv22032 <150386492+nv22032@users.noreply.github.com>
2024-03-30 09:13:55 +0000

    24 & 25 - Login Page and User Profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,15 +4,11 @@ import {
   registerUser,
   loginUser,
   userProfile,
-  updateProfile,
-  updateProfilePicture,
 } from "../controllers/userControllers";
 import { authGuard } from "../middleware/authMiddleware";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authGuard, userProfile);
-router.put("/updateProfile", authGuard, updateProfile);
-router.put("/updateProfilePicture", authGuard, updateProfilePicture);
 
 export default router;
